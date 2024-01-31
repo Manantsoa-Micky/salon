bced0a86db2bbe58e1d7a52906e46b33fa794334
Manantsoa-Micky <mickymickymarc@gmail.com>
2024-01-31 18:15:18 +0300

    added custom logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const { errorHandler } = require('./middlewares/errorHandler');
+require('./utils/loggers');
+const winston = require('winston');
+
+const logger = winston.loggers.get('simpleLogger');
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+const DATABASE_URL = process.env.DATABASE_URL;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`);
+mongoose.connect(DATABASE_URL).then(function () {
+    logger.info('⚡ Connected to DB...');
+});
+app.listen(PORT, () => {
+    logger.info('⚡ Connected to DB...');
 });
---
+++ src/utils/loggers.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+const { combine, timestamp, json, prettyPrint, errors } = winston.format;
+
+winston.loggers.add('testLogger', {
+    level: 'debug',
+    format: combine(
+        errors({ stack: true }),
+        timestamp(),
+        json(),
+        prettyPrint()
+    ),
+    transports: [new winston.transports.Console()],
+    defaultMeta: { service: 'Test Service' },
+});
+
+winston.loggers.add('simpleLogger', {
+    level: 'info',
+    format: winston.format.cli(),
+    transports: [new winston.transports.Console()],
+});
